7646234159d6f02656a9268775a94def32a014e3
Mr.JoZ <sakchai@s-planet.co.th>
2021-01-21 21:28:37 +0700

    programmer_test1

--- Programmer_test1/function.php
+++ Programmer_test1/function.php
@@ -1,29 +1,29 @@
 
-<?php 
+<?php
 
 if($_GET['type_search']){
-    
+
     if($_GET['type_search'] == 'Linear'){
-        Linear($_POST['search_list_number'], $_POST['search_number']);
+        Linear_search($_POST['search_list_number'], $_POST['search_number']);
     }
 
     if($_GET['type_search'] == 'Binary'){
-        Binary($_POST['search_list_number'], $_POST['search_number']);
+        Binary_search($_POST['search_list_number'], $_POST['search_number']);
     }
 
     if($_GET['type_search'] == 'Bubble'){
-        Bubble($_POST['search_list_number']);
+        Bubble_sort($_POST['search_list_number']);
     }
 
 }
 
-function Linear($search_list_number, $search_number){
+function Linear_search($search_list_number, $search_number){
     $output = [];
     $output['error'] = '0';
     $output['msg'] = 'OK.';
     $output['result'] = 'Result !!!!<br>';
     $round = 1;
-    
+
     $arry_search_list_number = explode(",", $search_list_number);
     foreach($arry_search_list_number AS $keys => $value){
         if($value != ''){
@@ -34,60 +34,55 @@ function Linear($search_list_number, $search_number){
                 break;
             }
             $round++;
-        } 
+        }
     }
-    
-
     echo json_encode($output);
-    
-    
 }
 
-function Binary($search_list_number, $search_number){
+function Binary_search($search_list_number, $search_number){
     $output = [];
     $output['error'] = '0';
     $output['msg'] = 'OK.';
     $output['result'] = 'Result !!!!<br>';
 
     $arry_search_list_number = explode(",", $search_list_number);
-    sort($arry_search_list_number, SORT_NUMERIC); 
+    sort($arry_search_list_number, SORT_NUMERIC);
 
     $output['result'] .= 'Sort : [ '.implode(",",$arry_search_list_number).' ] <br>';
-  
-    $low = 0; 
-    $high = count($arry_search_list_number) - 1; 
+
+    $low = 0;
+    $high = count($arry_search_list_number) - 1;
     $round = 1;
 
-    while ($low <= $high) { 
-            $mid = floor(($low + $high) / 2); 
-            if($search_number != $arry_search_list_number[$mid]){
-                $output['result'] .= 'Round : '.$round.' ===> '.' low = '.$low.', high = '.$high.', Mid = '.$mid.  ' ===> '. $arry_search_list_number[$mid] .' != '. $search_number.'<br>';
-                if($search_number < $arry_search_list_number[$mid]){
-                    $high = $mid -1; 
-                }else if($search_number > $arry_search_list_number[$mid]){
-                    $low = $mid + 1; 
-                }
-            }else if($search_number == $arry_search_list_number[$mid] ){
-                $output['result'] .= 'Round : '.$round.' ===> '.', low = '.$low.', high = '.$high.' Mid = '.$mid.  ' ===> '. $arry_search_list_number[$mid] .' = '. $search_number.' found!!';
-                break;
+    while ($low <= $high) {
+        $mid = floor(($low + $high) / 2);
+        if($search_number != $arry_search_list_number[$mid]){
+            $output['result'] .= 'Round : '.$round.' ===> '.' low = '.$low.', high = '.$high.', Mid = '.$mid.  ' ===> '. $arry_search_list_number[$mid] .' != '. $search_number.'<br>';
+            if($search_number < $arry_search_list_number[$mid]){
+                $high = $mid -1;
+            }else if($search_number > $arry_search_list_number[$mid]){
+                $low = $mid + 1;
             }
-            $round++;  
+        }else if($search_number == $arry_search_list_number[$mid] ){
+            $output['result'] .= 'Round : '.$round.' ===> '.', low = '.$low.', high = '.$high.' Mid = '.$mid.  ' ===> '. $arry_search_list_number[$mid] .' = '. $search_number.' found!!';
+            break;
+        }
+        $round++;
     }
-   
-    echo json_encode($output);  
+    echo json_encode($output);
 }
 
-function Bubble($search_list_number){
+function Bubble_sort($search_list_number){
     $output = [];
     $output['error'] = '0';
     $output['msg'] = 'OK.';
     $output['result'] = 'Result !!!!<br>';
 
-    $arry_search_list_number = explode(",", $search_list_number); 
+    $arry_search_list_number = explode(",", $search_list_number);
     $round = 1;
     $count_arry_search_list_number = count($arry_search_list_number)-1;
 
-    
+
     for ($i=0; $i<$count_arry_search_list_number; $i++) {
         for ($j=0; $j<$count_arry_search_list_number-$i; $j++) {
             $k = $j+1;
@@ -101,8 +96,7 @@ function Bubble($search_list_number){
             $round++;
         }
     }
-    echo json_encode($output);  
-  
+    echo json_encode($output);
 }
 
 ?>
--- Programmer_test1/index.php
+++ Programmer_test1/index.php
@@ -1,155 +1,167 @@
 
 <html>
-    <head> <title> Test_SHIPPON </title> 
-        <link rel="shortcut icon" href="image/logo_company.jfif" type="image/png" >
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<head> <title> Test_SHIPPON </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-         <!--  icon-->
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!--  icon-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-         <!-- sweet alert -->
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <!-- sweet alert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+</head>
 
+<!--  ///////////////////////////////////////////////////////// CSS ///////////////////////////////////////////// -->
+<style>
+    *{
+        font-size: 16px;
+        font-family: Cloud;
+    }
+    .top_row{
+        margin-top: 15px
+    }
+    .result_search{
+        padding: 10px;
+        border: 1px solid black;
+        height: 500px;
+        text-align: center;
+        font-size:18px;
+    }
+    .overflow_result{
+        overflow: auto;
+        height: 380px;
+        margin-top: 10px;
+    }
 
-    </head>
+</style>
 
-    <!--  ///////////////////////////////////////////////////////// CSS ///////////////////////////////////////////// -->
-    <style>
-        *{
-            font-size: 16px;
-            font-family: Cloud;
-        }
-      
-    </style>
+<!--  ///////////////////////////////////////////////////////// html ///////////////////////////////////////////// -->
+<body >
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3" >
 
-   <!--  ///////////////////////////////////////////////////////// html ///////////////////////////////////////////// -->
-    <body >
-        <div class="container-fluid">
+        </div>
+        <div class="col-sm-6" style="padding: 50px; border: 1px solid black">
             <div class="row">
-                <div class="col-sm-3" >
-                
+                <div class="col-sm-2" >
+                    <label style="float:right">List</label>
                 </div>
-                <div class="col-sm-6" style="padding: 50px; border: 1px solid black">
-                    <div class="row">
-                        <div class="col-sm-2" >
-                            <label style="float:right">List</label>
-                        </div>
-                        <div class="col-sm-9" >
-                            <input type="text" class="form-control" id="search_list_number">
-                        </div>
-                    </div>
-                    <div class="row" style="margin-top: 15px">
-                        <div class="col-sm-2" >
-                            <label style="float:right">ค้นหา</label>
-                        </div>
-                        <div class="col-sm-5" >
-                            <input type="text" class="form-control" id="search_number">
-                        </div>
-                        <div class="col-sm-3" >
-                            <button class="btn btn-warning" onclick="search()">
-                            <i class="fa fa-search" aria-hidden="true"></i> ค้นหา 
-                            </button>
-                        </div>
-                    </div>
-                    <div class="row" style="margin-top: 15px">
-                        <div class="col-sm-12" >
-                            <h3 class="text-center">ประเภทการค้นหา</h3>
-                        </div>
-                        <div class="col-sm-2" >
-                            
-                        </div>
-                        <div class="col-sm-7" >
-                            <select class="form-control"
-                                    id="type_search">
-                                    <option value="Linear">Linear Search</option>
-                                    <option value="Binary">Binary Search</option>
-                                    <option value="Bubble">Bubble Sort</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-3" >
-                            
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-2" >
-                            
-                        </div>
-                        <div class="col-sm-10" >
-                            <span class="text-danger">***จากการศึกษาไม่รู้จัก Bubble Search เลยทำ Bubble Sort แทน</span>
-                        </div>
-                    </div>
-                    <div class="row" style="margin-top: 15px">
-                        <div class="col-sm-1" >
-                            
-                        </div>
-                        <div class="col-sm-11">
-                            <h2 style="margin-left: -15px">ผลลัพธ์</h2>
-                        </div>
-                        <div class="col-sm-1" >
-                            
-                        </div>
-                        <div class="col-sm-10" style="padding: 10px; border: 1px solid black; height: 500px; text-align: center" >
-                            <lable id="show_list_number"></lable><br>
-                            <lable id="show_number"></lable><br>
-                            <div style="overflow: auto; font-size:18px;height: 380px; margin-top: 10px">
-                                <strong id="show_sesult" ><br></strong>
-                            <div>
+                <div class="col-sm-9" >
+                    <input type="text" class="form-control" id="search_list_number">
+                </div>
+            </div>
+            <div class="row top_row">
+                <div class="col-sm-2" >
+                    <label style="float:right">ค้นหา</label>
+                </div>
+                <div class="col-sm-5" >
+                    <input type="text" class="form-control" id="search_number">
+                </div>
+                <div class="col-sm-3" >
+                    <button class="btn btn-warning" onclick="search()">
+                        <i class="fa fa-search" aria-hidden="true"></i> ค้นหา
+                    </button>
+                </div>
+            </div>
+            <div class="row top_row" >
+                <div class="col-sm-12" >
+                    <h3 class="text-center">ประเภทการค้นหา</h3>
+                </div>
+                <div class="col-sm-2" >
+
+                </div>
+                <div class="col-sm-7" >
+                    <select class="form-control"
+                            id="type_search">
+                        <option value="Linear">Linear Search</option>
+                        <option value="Binary">Binary Search</option>
+                        <option value="Bubble">Bubble Sort</option>
+                    </select>
+                </div>
+                <div class="col-sm-3" >
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2" >
+
+                </div>
+                <div class="col-sm-10" >
+                    <span class="text-danger">***จากการศึกษาไม่รู้จัก Bubble Search เลยทำ Bubble Sort แทน</span>
+                </div>
+            </div>
+            <div class="row top_row" >
+                <div class="col-sm-1" >
+
+                </div>
+                <div class="col-sm-11">
+                    <h2 style="margin-left: -15px">ผลลัพธ์</h2>
+                </div>
+                <div class="col-sm-1" >
+
+                </div>
+                <div class="col-sm-10  result_search">
+                    <lable id="show_list_number"></lable><br>
+                    <lable id="show_number"></lable><br>
+                    <div class="overflow_result">
+                        <strong id="show_result" ><br></strong>
+                        <div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </body>
+</body>
 
 
-    <!--////////////////////////////////////////////////  JS /////////////////////////////////////////// -->
-    <script>
-        $(document).ready(function(){    
-            $('#search_number').on('input', function (event) {
-                this.value = this.value.replace(/[^0-9]/g, '');
-            });
-
-            $('#search_list_number').on('input', function (event) {
-                this.value = this.value.replace(/[^0-9,]/g, '');
-            });
+<!--////////////////////////////////////////////////  JS /////////////////////////////////////////// -->
+<script>
+    $(document).ready(function(){
+        $('#search_number').on('input', function (event) {
+            this.value = this.value.replace(/[^0-9]/g, '');
         });
 
+        $('#search_list_number').on('input', function (event) {
+            this.value = this.value.replace(/[^0-9,]/g, '');
+        });
+    });
 
-        function search(){
-            var type_search = $('#type_search').val();
-            var search_list_number = $('#search_list_number').val();
-            var search_number = $('#search_number').val();
 
-            if(search_list_number == '' || search_number == ''){
-                Swal.fire('กรุณากรอก List และ เลขค้นหา', '', 'error').then(function (result) {
-                    if (result.value) {
-                        return false;
-                    } 
-                });
-            }else{
-                $('#show_list_number').text('List : [ '+search_list_number+' ]');
-                $('#show_number').text('Search : '+search_number);
-                $.ajax({
-                    type: "POST",
-                    url: 'function.php?type_search='+type_search,
-                    data: {
-                        search_list_number : search_list_number,
-                        search_number : search_number
-                    },
-                    success: function(res){
-                        var data = JSON.parse(res);
-                        if(data.error = "0"){
-                            $('#show_sesult').html(data.result);
-                        }else{
+    function search(){
+        var type_search = $('#type_search').val();
+        var search_list_number = $('#search_list_number').val();
+        var search_number = $('#search_number').val();
 
-                        }
+        if(search_list_number == '' || search_number == ''){
+            Swal.fire('กรุณากรอก List และ เลขค้นหา', '', 'error').then(function (result) {
+                if (result.value) {
+                    return false;
+                }
+            });
+        }else{
+            $('#show_list_number').text('List : [ '+search_list_number+' ]');
+            $('#show_number').text('Search : '+search_number);
+            $.ajax({
+                type: "POST",
+                url: 'function.php?type_search='+type_search,
+                data: {
+                    search_list_number : search_list_number,
+                    search_number : search_number
+                },
+                success: function(res){
+                    var data = JSON.parse(res);
+                    if(data.error = "0"){
+                        $('#show_result').html(data.result);
+                    }else{
+                        $('#show_result').html('เกิดข้อผิดพลาดกรุณาลองใหม่');
                     }
-                });
-            }
+                }
+            });
         }
+    }
 
 
-    </script>
+</script>
 </html>
